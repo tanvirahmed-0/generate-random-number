e43bf965e07f9c8c24e03d401d96ac01c5abbc0d
Tanvir Ahmed <93043879+tanvirahmed-0@users.noreply.github.com>
2021-12-08 15:27:34 +0600

    Add files via upload

---
+++ Random Number/index.js
@@ -0,0 +1,9 @@
+
+function myfunction(){
+    Math.random();
+    Math.min(1000);
+
+
+var theRandomNumber = Math.floor(Math.random() * 10000) + 1;
+document.getElementById('demo').innerHTML= theRandomNumber;
+}
